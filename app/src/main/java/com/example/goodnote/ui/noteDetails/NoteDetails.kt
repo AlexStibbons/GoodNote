b2c861e9e04a123b4fa1b94ebb47e50aefc71973
AlexStibbons <43070613+AlexStibbons@users.noreply.github.com>
2020-01-08 10:55:53 +0100

    remove old add tags functionality from note details activity

--- app/src/main/java/com/example/goodnote/ui/noteDetails/NoteDetails.kt
+++ app/src/main/java/com/example/goodnote/ui/noteDetails/NoteDetails.kt
@@ -25,14 +25,11 @@ class NoteDetails : AppCompatActivity() {
 
    // lateinit var title: EditText
     lateinit var text: EditText
-    lateinit var chipGroup: ChipGroup
     lateinit var addTagsBtn: Button
     lateinit var tagsText: TextView
-    lateinit var autocomplete: AutoCompleteTextView
     lateinit var binding: NotesDetailsActivityBinding
 
     private lateinit var noteDetailsViewModel: NoteDetailsViewModel
-    private var allTags: MutableList<TagModel> = ArrayList()
     private var note = NoteDetailsModel("-1", "", "", mutableListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,17 +50,8 @@ class NoteDetails : AppCompatActivity() {
             Log.e("DETAILS", "GET called")
         })
 
-        noteDetailsViewModel.existingTags.observe(this, Observer {
-            it ?: return@Observer
-            allTags.clear()
-            allTags.addAll(it)
-            setupAutocomplete()
-        })
-
        // title = findViewById(R.id.notes_details_title)
         text = findViewById(R.id.notes_details_text)
-        chipGroup = findViewById(R.id.notes_details_tags_group)
-        autocomplete = findViewById(R.id.notes_details_autocomplete)
         addTagsBtn = findViewById(R.id.add_tags_button)
         tagsText = findViewById(R.id.tags_text)
 
@@ -72,7 +60,6 @@ class NoteDetails : AppCompatActivity() {
             val dialog: AddTagsDialogFragment = AddTagsDialogFragment.getInstance(noteDetailsViewModel, noteId)
             dialog.show(supportFragmentManager, "")
         }
-
     }
 
     override fun onBackPressed() {
@@ -93,73 +80,6 @@ class NoteDetails : AppCompatActivity() {
         binding.notesDetailsTitle.setText(note.title)
         tagsText.text =  note.tags.toTagsString().ifBlank { "No tags yet!" }
         text.setText(note.text)
-        chipGroup.removeAllViews()
-        note.tags.forEach {
-            addChip(it)
-        }
     }
 
-    private fun addTag(name: String) {
-        if (name.isBlank()) return
-
-        if (allTags.none { it.name == name }) {
-            val newTag = TagModel(name = name)
-            noteDetailsViewModel.saveTag(newTag)
-            noteDetailsViewModel.addTagForNote(note.noteId, newTag)
-        }
-
-        if (allTags.any { it.name == name }) {
-            val tag = allTags.first { it.name == name }
-            noteDetailsViewModel.addTagForNote(note.noteId, tag)
-            Log.e("ADD TAG", "${note.title} and id ${note.noteId}, ${tag.name}")
-        }
-    }
-
-    private fun addChip(tag: TagModel) {
-        val chip = Chip(this).apply {
-            text = tag.name
-            isCloseIconVisible = true
-            setOnCloseIconClickListener {
-                noteDetailsViewModel.deleteTagForNote(note.noteId, tag.tagId)
-            }
-        }
-        binding.notesDetailsTagsGroup.addView(chip)
-    }
-
-    private fun setupAutocomplete() {
-
-        val autoAdapter = ArrayAdapter<TagModel>(
-            this,
-            R.layout.support_simple_spinner_dropdown_item,
-            allTags
-        )
-
-        autocomplete.setAdapter(autoAdapter)
-
-        autocomplete.setOnItemClickListener { adapterView, _, position, _ ->
-            autocomplete.text = null
-            val tag: TagModel = adapterView.getItemAtPosition(position) as TagModel
-            noteDetailsViewModel.addTagForNote(note.noteId, tag)
-        }
-
-        autocomplete.addTextChangedListener {
-            if (it?.isEmpty() == true) return@addTextChangedListener
-
-            if (it?.last() == ',') {
-                val name = it.substring(0, it.length - 1)
-                addTag(name)
-                autocomplete.text = null
-            }
-        }
-
-        autocomplete.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                val name = v.text.toString()
-                v.text = null
-                addTag(name)
-                return@setOnEditorActionListener true
-            }
-            false
-        }
-    }
 }
